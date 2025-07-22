6589c71b157394224524464ff6ece29f2a695db0
Gabcat13 <shawntetay1@gmail.com>
2025-07-22 04:40:58 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,22 @@
+// backend/db.js
+import fs from 'fs';
+const DB_FILE = './db.json';
+
+export function readDB() {
+  try {
+    const data = fs.readFileSync(DB_FILE, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Error reading DB:', err);
+    return {};
+  }
+}
+
+export function writeDB(data) {
+  try {
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error('Error writing DB:', err);
+  }
+}
 
